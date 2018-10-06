f0d9903dca803e70b6e579e5dcb99a93018b07c8
Alexey <gusevalexey0@gmail.com>
2018-10-06 13:52:10 +0300

    fix

--- compression.h
+++ compression.h
@@ -20,7 +20,4 @@ std::vector<uint64_t> buildCodes(std::vector<unsigned char> const &lens);
 void countLens(size_t v, unsigned char d, std::vector<node> const &tree, std::vector<unsigned char> &lens);
 void encode(std::istream &input, std::ostream &output);
 void decode(std::istream &input, std::ostream &output);
-//void countLens(size_t v, uint16_t d, std::vector<node> const &tree, std::vector<unsigned char> &lens);
-//std::vector<uint64_t> buildCodes(std::vector<unsigned char> const &lens);
-//void encode(std::istream &source, std::ostream &destination);
 #endif //HUFFMAN_COMPRESS_H
